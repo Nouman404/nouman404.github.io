110e419fd315d8ec7a848af7922367ec1e2023a6
Nouman404 <73934639+Nouman404@users.noreply.github.com>
2023-06-06 21:08:55 +0200

    Add files via upload

---
+++ _posts/CTFs/404CTF_2023/Crypto/ascon.py
@@ -0,0 +1,225 @@
+import binascii
+
+debug = False
+debugpermutation = False
+
+def ascon_decrypt(key, nonce, associateddata, ciphertext, variant="Ascon-128"):
+    """
+    Ascon decryption.
+    key: a bytes object of size 16 (for Ascon-128, Ascon-128a; 128-bit security) or 20 (for Ascon-80pq; 128-bit security)
+    nonce: a bytes object of size 16 (must not repeat for the same key!)
+    associateddata: a bytes object of arbitrary length
+    ciphertext: a bytes object of arbitrary length (also contains tag)
+    variant: "Ascon-128", "Ascon-128a", or "Ascon-80pq" (specifies key size, rate and number of rounds)
+    returns a bytes object containing the plaintext or None if verification fails
+    """
+    assert variant in ["Ascon-128", "Ascon-128a", "Ascon-80pq"]
+    if variant in ["Ascon-128", "Ascon-128a"]: assert(len(key) == 16 and len(nonce) == 16 and len(ciphertext) >= 16)
+    if variant == "Ascon-80pq": assert(len(key) == 20 and len(nonce) == 16 and len(ciphertext) >= 16)
+    S = [0, 0, 0, 0, 0]
+    k = len(key) * 8 # bits
+    a = 12 # rounds
+    b = 8 if variant == "Ascon-128a" else 6   # rounds
+    rate = 16 if variant == "Ascon-128a" else 8   # bytes
+
+    ascon_initialize(S, k, rate, a, b, key, nonce)
+    ascon_process_associated_data(S, b, rate, associateddata)
+    plaintext = ascon_process_ciphertext(S, b, rate, ciphertext[:-16])
+    tag = ascon_finalize(S, rate, a, key)
+    if tag == ciphertext[-16:]:
+        return plaintext
+    else:
+        return None
+
+
+# === Ascon AEAD building blocks ===
+def ascon_initialize(S, k, rate, a, b, key, nonce):
+    """
+    Ascon initialization phase - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    k: key size in bits
+    rate: block size in bytes (8 for Ascon-128, Ascon-80pq; 16 for Ascon-128a)
+    a: number of initialization/finalization rounds for permutation
+    b: number of intermediate rounds for permutation
+    key: a bytes object of size 16 (for Ascon-128, Ascon-128a; 128-bit security) or 20 (for Ascon-80pq; 128-bit security)
+    nonce: a bytes object of size 16
+    returns nothing, updates S
+    """
+    iv_zero_key_nonce = to_bytes([k, rate * 8, a, b] + (20-len(key))*[0]) + key + nonce
+    S[0], S[1], S[2], S[3], S[4] = bytes_to_state(iv_zero_key_nonce)
+    if debug: printstate(S, "initial value:")
+
+    ascon_permutation(S, a)
+
+    zero_key = bytes_to_state(zero_bytes(40-len(key)) + key)
+    S[0] ^= zero_key[0]
+    S[1] ^= zero_key[1]
+    S[2] ^= zero_key[2]
+    S[3] ^= zero_key[3]
+    S[4] ^= zero_key[4]
+    if debug: printstate(S, "initialization:")
+
+
+def ascon_process_associated_data(S, b, rate, associateddata):
+    """
+    Ascon associated data processing phase - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    b: number of intermediate rounds for permutation
+    rate: block size in bytes (8 for Ascon-128, 16 for Ascon-128a)
+    associateddata: a bytes object of arbitrary length
+    returns nothing, updates S
+    """
+    if len(associateddata) > 0:
+        a_zeros = rate - (len(associateddata) % rate) - 1
+        a_padding = to_bytes([0x80] + [0 for i in range(a_zeros)])
+        a_padded = associateddata + a_padding
+
+        for block in range(0, len(a_padded), rate):
+            S[0] ^= bytes_to_int(a_padded[block:block+8])
+            if rate == 16:
+                S[1] ^= bytes_to_int(a_padded[block+8:block+16])
+
+            ascon_permutation(S, b)
+
+    S[4] ^= 1
+    if debug: printstate(S, "process associated data:")
+
+
+def ascon_process_ciphertext(S, b, rate, ciphertext):
+    """
+    Ascon ciphertext processing phase (during decryption) - internal helper function. 
+    S: Ascon state, a list of 5 64-bit integers
+    b: number of intermediate rounds for permutation
+    rate: block size in bytes (8 for Ascon-128, Ascon-80pq; 16 for Ascon-128a)
+    ciphertext: a bytes object of arbitrary length
+    returns the plaintext, updates S
+    """
+    c_lastlen = len(ciphertext) % rate
+    c_padded = ciphertext + zero_bytes(rate - c_lastlen)
+
+    # first t-1 blocks
+    plaintext = to_bytes([])
+    for block in range(0, len(c_padded) - rate, rate):
+        if rate == 8:
+            Ci = bytes_to_int(c_padded[block:block+8])
+            plaintext += int_to_bytes(S[0] ^ Ci, 8)
+            S[0] = Ci
+        elif rate == 16:
+            Ci = (bytes_to_int(c_padded[block:block+8]), bytes_to_int(c_padded[block+8:block+16]))
+            plaintext += (int_to_bytes(S[0] ^ Ci[0], 8) + int_to_bytes(S[1] ^ Ci[1], 8))
+            S[0] = Ci[0]
+            S[1] = Ci[1]
+
+        ascon_permutation(S, b)
+
+    # last block t
+    block = len(c_padded) - rate
+    if rate == 8:
+        c_padding1 = (0x80 << (rate-c_lastlen-1)*8)
+        c_mask = (0xFFFFFFFFFFFFFFFF >> (c_lastlen*8))
+        Ci = bytes_to_int(c_padded[block:block+8])
+        plaintext += int_to_bytes(Ci ^ S[0], 8)[:c_lastlen]
+        S[0] = Ci ^ (S[0] & c_mask) ^ c_padding1
+    elif rate == 16:
+        c_lastlen_word = c_lastlen % 8
+        c_padding1 = (0x80 << (8-c_lastlen_word-1)*8)
+        c_mask = (0xFFFFFFFFFFFFFFFF >> (c_lastlen_word*8))
+        Ci = (bytes_to_int(c_padded[block:block+8]), bytes_to_int(c_padded[block+8:block+16]))
+        plaintext += (int_to_bytes(S[0] ^ Ci[0], 8) + int_to_bytes(S[1] ^ Ci[1], 8))[:c_lastlen]
+        if c_lastlen < 8:
+            S[0] = Ci[0] ^ (S[0] & c_mask) ^ c_padding1
+        else:
+            S[0] = Ci[0]
+            S[1] = Ci[1] ^ (S[1] & c_mask) ^ c_padding1
+    if debug: printstate(S, "process ciphertext:")
+    return plaintext
+
+
+def ascon_finalize(S, rate, a, key):
+    """
+    Ascon finalization phase - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    rate: block size in bytes (8 for Ascon-128, Ascon-80pq; 16 for Ascon-128a)
+    a: number of initialization/finalization rounds for permutation
+    key: a bytes object of size 16 (for Ascon-128, Ascon-128a; 128-bit security) or 20 (for Ascon-80pq; 128-bit security)
+    returns the tag, updates S
+    """
+    assert(len(key) in [16,20])
+    S[rate//8+0] ^= bytes_to_int(key[0:8])
+    S[rate//8+1] ^= bytes_to_int(key[8:16])
+    S[rate//8+2] ^= bytes_to_int(key[16:] + zero_bytes(24-len(key)))
+
+    ascon_permutation(S, a)
+
+    S[3] ^= bytes_to_int(key[-16:-8])
+    S[4] ^= bytes_to_int(key[-8:])
+    tag = int_to_bytes(S[3], 8) + int_to_bytes(S[4], 8)
+    if debug: printstate(S, "finalization:")
+    return tag
+
+
+# === Ascon permutation ===
+def ascon_permutation(S, rounds=1):
+    """
+    Ascon core permutation for the sponge construction - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    rounds: number of rounds to perform
+    returns nothing, updates S
+    """
+    assert(rounds <= 12)
+    if debugpermutation: printwords(S, "permutation input:")
+    for r in range(12-rounds, 12):
+        # --- add round constants ---
+        S[2] ^= (0xf0 - r*0x10 + r*0x1)
+        if debugpermutation: printwords(S, "round constant addition:")
+        # --- substitution layer ---
+        S[0] ^= S[4]
+        S[4] ^= S[3]
+        S[2] ^= S[1]
+        T = [(S[i] ^ 0xFFFFFFFFFFFFFFFF) & S[(i+1)%5] for i in range(5)]
+        for i in range(5):
+            S[i] ^= T[(i+1)%5]
+        S[1] ^= S[0]
+        S[0] ^= S[4]
+        S[3] ^= S[2]
+        S[2] ^= 0XFFFFFFFFFFFFFFFF
+        if debugpermutation: printwords(S, "substitution layer:")
+        # --- linear diffusion layer ---
+        S[0] ^= rotr(S[0], 19) ^ rotr(S[0], 28)
+        S[1] ^= rotr(S[1], 61) ^ rotr(S[1], 39)
+        S[2] ^= rotr(S[2],  1) ^ rotr(S[2],  6)
+        S[3] ^= rotr(S[3], 10) ^ rotr(S[3], 17)
+        S[4] ^= rotr(S[4],  7) ^ rotr(S[4], 41)
+        if debugpermutation: printwords(S, "linear diffusion layer:")
+
+
+# === helper functions ===
+def zero_bytes(n):
+    return n * b"\x00"
+
+def to_bytes(l): # where l is a list or bytearray or bytes
+    return bytes(bytearray(l))
+
+def bytes_to_int(bytes):
+    return sum([bi << ((len(bytes) - 1 - i)*8) for i, bi in enumerate(to_bytes(bytes))])
+
+def bytes_to_state(bytes):
+    return [bytes_to_int(bytes[8*w:8*(w+1)]) for w in range(5)]
+
+def int_to_bytes(integer, nbytes):
+    return to_bytes([(integer >> ((nbytes - 1 - i) * 8)) % 256 for i in range(nbytes)])
+
+def rotr(val, r):
+    return (val >> r) | ((val & (1<<r)-1) << (64-r))
+
+
+
+key = binascii.unhexlify('00456c6c616e61206427416c2d466172')
+nonce = b'\x00'*16
+
+ciphertext = binascii.unhexlify('ac6679386ffcc3f82d6fec9556202a1be26b8af8eecab98783d08235bfca263793b61997244e785f5cf96e419a23f9b29137d820aab766ce986092180f1f5a690dc7767ef1df76e13315a5c8b04fb782')
+associated_data = binascii.unhexlify('80400c0600000000')
+
+decrypted_message = ascon_decrypt(key, nonce, associated_data, ciphertext)
+
+print("Decrypted message:", decrypted_message.replace(b'\xe8', b'\xc3\xa8').decode())
